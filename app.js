458436777ba6cf1428ec8067e3c0cad58e0f634e
calebthewood <calebwood.cs@gmail.com>
2022-03-15 17:21:54 -0700

    adds remove funcionality, changes search btn to submit, improves variables.

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 "use strict";
 
 console.log("Let's get this party started!");
+const $DISPLAY = $(".display");
+const api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const giphyUrl = "http://api.giphy.com/v1/gifs/search";
 
-function showHand(hand) {
-    let $box = $("#hand");
-    $box.empty();
-  
-    for (let {rank, suit} of hand) {
-      let t = `<p>${rank} of ${suit}</p>`;
-      $box.append($(t));
-    }
-  }
-  
-  async function getGif(evt) {
-      evt.preventDefault()
-    let $gifName = $(".search-text").val();
-    console.log($gifName)
-    let api_key = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
-    let response = await axios.get(
-      "giphy.com/search", {param: {q: `${$gifName}`, api_key}});
-    console.log("get Gif response=", response);
-    showHand(response.data.hand);
-  }
-  
-  $(".search").on("click", getGif);
+/**
+ * Clear all appended gifs from the DOM.
+ */
+function removeGif() {
+  $DISPLAY.empty();
+}
+
+/**
+ * Accepts gif url, and appends gif to DOM element.
+ */
+function showGif(gif) {
+  $DISPLAY.append($(`<img src="${gif}" placeholder="gif">`));
+}
+
+/**
+ * Makes a GET request to Giphy API using user submitted keyword. Pulls 1st result from response and passes to showGif.
+ */
+async function getGif(evt) {
+  evt.preventDefault();
+
+  const $gifName = $(".search-text").val();
+  console.log($gifName);
+
+  const response = await axios.get(
+    giphyUrl, { params: { q: $gifName, api_key } }
+    );
+
+  const gifUrl = response.data.data[0].images.original.url;
+  console.log("get Gif response=", gifUrl);
+  showGif(gifUrl);
+}
+
+$(".submit").on("click", getGif);
+$(".remove-images").on("click", removeGif);
